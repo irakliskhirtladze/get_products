b1cfdab6e5d46c37f505882bc09bd301a80a82bc
irakliskhirtladze <iskhirtladzei@gmail.com>
2024-03-03 18:34:59 +0400

    initial commit

---
+++ products.py
@@ -0,0 +1,64 @@
+import requests
+import json
+import sys
+import time
+import threading
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QIcon
+
+
+class MainWindow(QMainWindow):
+    
+    def __init__(self) -> None:
+        """Initialize UI, a list to store products and connect a button with method"""
+        super().__init__()
+        self.ui = loadUi("ui/products.ui", self)
+        
+        self.ui.pushButton.clicked.connect(self.get_products)
+        self.products = []
+
+    def get_product(self, url) -> None:
+        """Obtains single product from given URL"""
+        response = requests.get(url)
+        dictionary = response.json()
+        self.products.append(dictionary) 
+
+    def get_products(self) -> None:
+        """Obtains products from given number of URLs in parallel and writes them to JSON"""
+        self.num_of_products = int(self.ui.spinBox.text())
+        self.urls = [f'https://dummyjson.com/products/{str(i)}' for i in range(1, self.num_of_products)]
+        threads = [] # Stores threads before they are joined
+
+        try:
+            start_time = time.perf_counter()
+            for url in self.urls:
+                thread = threading.Thread(target=self.get_product, args=(url,))
+                thread.start()
+                threads.append(thread)
+            
+            for thread in threads:
+                thread.join()
+            end_time = time.perf_counter()
+
+            with open('products.json', 'w') as f:
+                json.dump(self.products, f, indent=4)
+
+            # Set labels
+            self.ui.label.setStyleSheet("color:rgb(0, 255, 127)")
+            self.ui.label.setText("Success")
+            self.ui.label_2.setText(f"Obtained {self.num_of_products} products in {round(end_time - start_time, 3)} seconds")
+
+        except:
+            self.ui.label.setStyleSheet("color:rgb(255, 0, 0)")
+            self.ui.label.setText("Failure")
+            self.ui.label_2.setText(f"Could not obtain products")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.setWindowTitle("TBC Academy")
+    window.setWindowIcon(QIcon("resources/tbcicon.png"))
+    window.show()
+    sys.exit(app.exec_())
---
+++ readme.md
@@ -0,0 +1,3 @@
+This app demonstrates parallel processing using the example of sending requests to a webpage and getting response as JSON-formatted products.
+The number of requests (and therefore products) can be chosen between 1 and 100.
+Obtained products are stored in a list and then dumped to a "products.json" file.
